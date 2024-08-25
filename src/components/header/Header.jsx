46ce25136776a51e5a447fe6b879ab5a5bb6b590
ShinSieun <sisilver422@gmail.com>
2024-08-25 23:47:25 +0900

    [fe][feat] header css 적용 - 폰트 x

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import styles from './Header.module.css';
+import logo from '../../assets/로고.png'
+
+
 
 function Header() {
     const navigate = useNavigate();
-    
+
   return (
-    <header>    
-        <div>Header</div>
-        <button onClick={() => navigate('/')}>Go to Layout 1</button>
-        <button onClick={() => navigate('/page/pray')}>Go to Layout 2</button>
-        <button onClick={() => navigate('/page/education')}>Go to Layout 3</button>
-    </header>
+    <header className={styles.header}>
+    <div className={styles.logo}>
+      <img src={logo} alt="Logo" />
+      <h1>153 WORLD CHRISTIAN SCHOOL</h1>
+    </div>
+    <nav className={styles.menu}>
+    <a href="#" className={styles.menuItem}>학교 소개</a>
+        <a href="#" className={styles.menuItem}>입학안내</a>
+        <a href="#" className={styles.menuItem}>교육과정</a>
+        <a href="#" className={styles.menuItem}>학교생활</a>
+        <a href="#" className={styles.menuItem}>게시판</a>
+    </nav>
+  </header>
 
   )
 }
