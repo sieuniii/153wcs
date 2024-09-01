23c4c1c923f5197c89c5799a5b7c346532cd8797
ShinSieun <nssu422@naver.com>
2024-09-01 16:50:24 +0900

    [fe][feat] main 화면 1차 끝!
    todo : 페이지 연결 되면 연결 할 것
    todo : 인터뷰 완료 할것

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,26 +8,50 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
+      <div className={styles.logo} onClick={() => navigate("/")}>
         <img src={logo} alt="Logo" />
         <p>153 WORLD CHRISTIAN SCHOOL</p>
       </div>
       <nav className={styles.menu}>
-        <a href="/" className={styles.menuItem}>
-          학교 소개
-        </a>
-        <a href="/" className={styles.menuItem}>
-          입학안내
-        </a>
-        <a href="/" className={styles.menuItem}>
-          교육과정
-        </a>
-        <a href="/" className={styles.menuItem}>
-          학교생활
-        </a>
-        <a href="/" className={styles.menuItem}>
-          게시판
-        </a>
+        <div className={styles.menuItem}>
+          <a href="#">학교소개</a>
+          <div className={styles.dropdownMenu}>
+            <a href="/history">학교장 소개</a>
+            <a href="/mission">153 기도문</a>
+            <a href="/team">153 캠퍼스 역사</a>
+            <a href="/team">학교 연혁</a>
+            <a href="/team">교직원 소개</a>
+            <a href="/team">학사 일정</a>
+            <a href="/team">연락처</a>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <a href="/">입학안내</a>
+        </div>
+        <div className={styles.menuItem}>
+          <a href="#">교육과정</a>
+          <div className={styles.dropdownMenu}>
+            <a href="/history">교육 철학</a>
+            <a href="/history">ABC/P.A.C.E</a>
+            <a href="/mission">초중등교육</a>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <a href="#">학교생활</a>
+          <div className={styles.dropdownMenu}>
+            <a href="/history">학교활동</a>
+            <a href="/mission">스쿨버스</a>
+            <a href="/team">갤러리</a>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          <a href="#">게시판</a>
+          <div className={styles.dropdownMenu}>
+            <a href="/history">공지사항</a>
+            <a href="/mission">뉴스</a>
+            <a href="/team">채용 공고</a>
+          </div>
+        </div>
       </nav>
     </header>
   );
