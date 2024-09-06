e6076af5cd677adb9429024d3f58e3e989f30352
ShinSieun <nssu422@naver.com>
2024-09-06 23:49:10 +0900

    [fe][fix] 배포 문제 수정

--- src/pages/introduction/PrayPage.jsx
+++ src/pages/introduction/PrayPage.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import TitleComponent from "../../components/common/TitleComponent";
 import BackgroundComponent from "../../components/common/BackgroundComponent";
 import image1 from "../../assets/img/학교소개.jpg";
-import styles from "./Praypage.module.css";
-import YoutubePreview from "../../components/common/YoutubePreview";
 
 function PrayPage() {
   return (
     <>
       <BackgroundComponent image={image1} />
-      <div className={styles.contentSection}>
-        {" "}
-        <TitleComponent text={"153 기도문"} />
-        <YoutubePreview videoId="DpxqxOFSsFk" />
-      </div>
     </>
   );
 }
